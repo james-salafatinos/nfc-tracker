693e6a5906b250799a72bcc7dd49b74716e345fc
james-salafatinos <jamessalafatinos@outlook.com>
2020-08-02 12:22:04 -0500

    Working habit viz

--- app_integrations/_get_habits.js
+++ app_integrations/_get_habits.js
@@ -1,4 +1,5 @@
 
+
 console.log('Running...')
 const cheerio = require('cheerio')
 const axios = require('axios')
@@ -22,9 +23,40 @@ const getHabits = async () => {
 }
 
 const convertHabits = async () =>{
+
+        //async return response and d3 parse
+        function _get_date_str(shift_days = 0){
+            const today = new Date()
+        
+            //shift the days if we want to get another date for easy time range
+            if (shift_days < 0){
+                today.setDate(today.getDate()+shift_days)
+            }
+                    
+            let strDate = 'm/d/Y'
+            .replace('Y', today.getFullYear())
+            .replace('m', today.getMonth()+1)
+            .replace('d', today.getDate());
+            return strDate
+        }
+        
     const response = await getHabits()
     let df = d3.csvParse(response.data, d3.autoType)
-    return df 
+
+    
+    //Create "Keep" filter for dates to display the 28 habit view
+    const date_entries = Array.from(Array(28).keys())
+    const dates_allowed = date_entries.map(date => _get_date_str(shift_days = -date));
+    filteredData = df.filter(function(d) {
+        //Filter data for last 4 weeks
+        if (dates_allowed.includes(d.CalendarDate)){
+            //return d.CalendarDate == _get_date_str(shift_days = -5)
+            return d
+        }
+    })
+    //console.log(filteredData)
+    
+    return filteredData
 }
 
 let df = convertHabits()
