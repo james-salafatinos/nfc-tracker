04206e3aa43faaaa272efe66c65bd301b05b9ad2
james-salafatinos <jamessalafatinos@outlook.com>
2020-08-17 14:06:40 -0500

    added new table to orig migration

--- migrations/20200728222224_create-app-table.js
+++ migrations/20200728222224_create-app-table.js
@@ -7,8 +7,15 @@ exports.up = function(knex) {
         tbl.text('value',600)
         tbl.timestamps(true,true)
     })
-    
-  };
+    .createTable('users', tbl => {
+      tbl.increments()
+      tbl.text('user', 200)
+      tbl.text('user_urls', 5000)
+      tbl.timestamps(true,true)
+  })
+  
+};
+  
   
 
 exports.down = function(knex) {
--- models/db_crud.js
+++ models/db_crud.js
@@ -1,3 +1,4 @@
+//Connects to database and determines which environment to use (development or prouction)
 const db = require('../db_config');
 
 module.exports = {
@@ -10,7 +11,13 @@ async function add(obj){
     return await db('logs').insert(obj, ['id'])
 
 }
-
+//find
 function find(){
     return db('logs')
+}
+
+function findByUser(id){
+    return db('logs')
+    .where({id})
+    .first()
 }
